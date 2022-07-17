a9022484ea450e34ebeae4a2f3175dfed8c99a3b
David Moncada <dmoncada5@gmail.com>
2022-07-17 17:05:33 -0600

    'igno5'

--- FrontEnd/src/environments/environment.ts
+++ FrontEnd/src/environments/environment.ts
@@ -1,7 +1,7 @@
 // This file can be replaced during build by using the `fileReplacements` array.
 // `ng build--prod` replaces `environment.ts` with `environment.prod.ts`.
 // The list of file replacements can be found in `angular.json`.
-//prueba1
+//prueba3
 export const environment = {
     production: true,
     hmr       : false,
