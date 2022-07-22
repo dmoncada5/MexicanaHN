da5618ce2255b070510e532ef77070724e9469bc
David Moncada <dmoncada5@gmail.com>
2022-07-22 11:38:52 -0600

    'actual'

--- FrontEnd/src/environments/environment.ts
+++ FrontEnd/src/environments/environment.ts
@@ -5,13 +5,8 @@
 export const environment = {
     production: true,
     hmr       : false,
-<<<<<<< HEAD
-  // ipKey:'http://localhost:5010/api'
+  //  ipKey:'http://localhost:5010/api'
     ipKey:'http://154.12.253.17:5010/api'
-=======
-    ipKey:'http://localhost:5010/api'
-    //ipKey:'http://154.12.253.17:5010/api'
->>>>>>> 04e50a7df8ce827154598b8b0726b98f05bd0957
 };
 
 /*
